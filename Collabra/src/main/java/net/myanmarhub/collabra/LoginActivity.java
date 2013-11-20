02d7f618572ce390cee95440da0930f6a8c2d379
Tin Htoo Aung <tinhtooaung@myanmarhub.net>
2013-11-20 23:24:34 +0630

    Commit Updates

--- Collabra/src/main/java/net/myanmarhub/collabra/LoginActivity.java
+++ Collabra/src/main/java/net/myanmarhub/collabra/LoginActivity.java
@@ -1,7 +1,18 @@
 package net.myanmarhub.collabra;
 
+import android.os.Bundle;
+
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+
 /**
  * Tin Htoo Aung (Myanmar Hub) on 20/11/13.
  */
-public class LoginActivity {
+public class LoginActivity extends SherlockFragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_login);
+    }
 }
